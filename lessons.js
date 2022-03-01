d15f2fd29976e27ea5bc2ed379fa152bd18193a2
uncleejay <joseph.c.o216@gmail.com>
2022-03-01 14:47:17 +0000

    updated manifest file

---
+++ lessons.js
@@ -0,0 +1,92 @@
+let lessons = [
+	{
+        id: 1,
+        subject: "Math",
+        location: "London",
+        price : 400,
+        image: "images/maths.jpg",
+        availablespace: 5,
+        rating: 5
+    },
+    {
+        id: 2,
+        subject: "English",
+        location: "Hendon",
+        price : 350,
+        image: "images/english.jpg",
+        availablespace: 5,
+        rating: 4
+    },
+    {
+        id: 3,
+        subject: "Art",
+        location: "Camden",
+        price : 450,
+        image: "images/art.jpg",
+        availablespace: 5,
+        rating: 5  
+    },
+    {
+        id: 4,
+        subject: "Science",
+        location: "Canning Town",
+        price : 600,
+        image: "images/science.jpg",
+        availablespace: 5,
+        rating: 5   
+    },
+    {
+        id: 5,
+        subject: "Geography",
+        location: "Westfield",
+        price : 550,
+        image: "images/geography.jpg",
+        availablespace: 5,
+        rating: 2   
+    },
+    {
+        id: 6,
+        subject: "Music",
+        location: "Stratford",
+        price : 250,
+        image: "images/music.jpg",
+        availablespace: 5,
+        rating: 3    
+    },
+    {
+        id: 7,
+        subject: "UI/UX",
+        location: "Westfield",
+        price : 450,
+        image: "images/ux.png",
+        availablespace: 5,
+        rating: 2   
+    },
+    {
+        id: 8,
+        subject: "Mobile Development",
+        location: "Chelsea",
+        price : 950,
+        image: "images/app.png",
+        availablespace: 5,
+        rating: 5 
+    },
+    {
+        id: 9,
+        subject: "Backend Development",
+        location: "Chelsea",
+        price : 950,
+        image: "images/web.jpg",
+        availablespace: 5,
+        rating: 4
+    },
+    {
+        id: 10,
+        subject: "Biology",
+        location: "Old Traford",
+        price : 450,
+        image: "images/biology.jpg",
+        availablespace: 5,
+        rating: 5      
+    },
+]
