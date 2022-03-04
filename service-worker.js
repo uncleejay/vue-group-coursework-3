2f018240755549d4b0ed38358ee439c738131919
uncleejay <joseph.c.o216@gmail.com>
2022-03-04 01:05:48 +0000

    updated service worker

--- service-worker.js
+++ service-worker.js
@@ -30,8 +30,15 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('fetch', function (e) {
     e.respondWith(
-        caches.match(e.request).then(function (e)  {
-            return e
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache,
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
         })
-    )
+    );
 });
